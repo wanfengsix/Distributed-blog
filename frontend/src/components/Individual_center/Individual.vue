9a5f71b638549791128b3419c7e304ab7d50ec88
cheatre <771294679@qq.com>
2023-12-05 12:07:06 +0800

    new file:   frontend/public/img/fdj.png 	new file:   frontend/public/img/fz.png 	new file:   frontend/public/img/grzx.png 	new file:   frontend/public/img/logo.png 	new file:   frontend/public/img/tx1.png 	new file:   frontend/public/img/tx2.png 	new file:   frontend/public/img/xff.png 	new file:   frontend/public/img/yh.png 	new file:   frontend/src/components/Individual_center/Individual.vue

---
+++ frontend/src/components/Individual_center/Individual.vue
@@ -0,0 +1,546 @@
+<template>
+	<head>
+		<meta charset="utf-8">
+		<link type="text/css" rel="stylesheet" href="css/css.css" />
+		<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+		<script type="text/javascript" src="js/base.js"></script>
+		<!--以下是页面独有JS,请不要放在公用php文件中-->
+
+		<!--以上是页面独有JS,请不要放在公用php文件中-->
+		<title></title>
+	</head>
+
+	<body>
+		<div class="head">
+			<div class="main">
+				<div class="head1">
+					<div class="logo">
+						<img src="img/logo.png">
+					</div>
+
+					<div class="geren">
+						<ul>
+							<li><a class="a1" href="###">首页</a></li>
+							<li><a class="a2" href="###">个人中心</a></li>
+							<div class="clear">
+
+							</div>
+						</ul>
+					</div>
+
+
+					<div class="input">
+						<input class="b1" type="text" name="" id="" value="" />
+						<a class="b2" href="###"><img src="img/fdj.png"></a>
+					</div>
+
+
+
+					<div class="san">
+						<a href="###">创作者中心</a>
+					</div>
+
+
+					<div class="si">
+						<img src="img/tx1.png">
+
+					</div>
+
+					<div class="wu">
+						<a href="###">退出登录</a>
+					</div>
+
+
+
+					<div class="clear">
+
+					</div>
+
+				</div>
+			</div>
+		</div>
+
+
+
+		<div class="centen">
+			<div class="main">
+
+				<div class="centen1">
+
+					<div class="left">
+						<span class="aw1">通知</span>
+						<p>“用户123456"给你的文章“鸿蒙OS起步照官网练习创建第一页面和跳转的第二页面需要注意的点”点赞了。<br><br>
+							“用户123456”给你的文章“鸿蒙OS起步照官网练习创建第一页面和跳转的第二页面需要注意的点”点赞了。<br><br>
+							“用户123456”给你的文章“鸿蒙OS起步照官网练习创建第一页面和跳转的第二页面需要注意的点”点赞了。<br><br>
+							“用户123456”给你的文章“鸿蒙OS起步照官网练习创建第—页面和跳转的第二页面需要注意的点”点赞了。<br><br>
+							“用户123456”给你的文章“鸿蒙OS起步照官网练习创建第一页面和跳转的第二页面需要注意的点”点赞了。<br><br>
+							“用户123456”给你的文章“鸿蒙OS起步照官网练习创建第一页面和跳转的第二页面需要注意的点”点赞了。<br><br>
+							“用户123456”给你的文章“鸿蒙OS起步照官网练习创建第一页面和跳转的第二页面需要注意的点”点赞了。<br><br>
+							“用户123456”给你的文章“鸿蒙OS起步照官网练习创建第一页面和跳转的第二页面需要注意的点”点赞了。<br><br>
+						</p>
+					</div>
+
+
+					<div class="zhong">
+						<div class="shang">
+							<div class="zuo">
+								<img src="img/tx2.png" >
+							</div>
+							
+							<div class="zhong">
+								<h3>用户1772145090487</h3>
+								<img src="img/yh.png" >
+								<span>个性签名</span>
+								<p>现在的我,仰观宇宙之大,俯察品类之盛，所以由目骋怀,亦足以极视听之娱,信可乐也。</p>
+							</div>
+							
+							<div class="you">
+								<a href="###">设置</a>
+							</div>
+							
+							<div class="clear">
+								
+							</div>
+							
+						</div>
+						
+						
+						<div class="xia">
+						<ul>
+							<li>
+							<div class="yi">
+								<h3>鸿蒙OS起步照官网练习创建第一页面和跳转的第二页面需要注意的点</h3>
+								<p>弹性伸缩具有应突发、省成本、自动化的业务价值。平台侧将各业务零散
+								、闲置资源进行整合,形成一个大规模资源池，通过弹性调度、
+								库存管控技术在公司运营成本和业务体感中寻求较好的平衡。</p>
+								<span>微信云</span> <span>2021-</span> <span>13</span> <span>13</span> <span>2.9</span><br>
+								<span>开发</span>   <span>03-24</span> <span>24</span> <span>24</span> <span>万</span>
+							</div>
+							<div class="er">
+								<img src="img/xff.png" >
+							</div>
+							
+							<div class="san">
+								<a href="###">修改</a>
+								<a href="###">删除</a>
+							</div>
+							<div class="clear">
+								
+							</div>
+							</li>
+							
+							<li>
+							<div class="yi">
+								<h3>鸿蒙OS起步照官网练习创建第一页面和跳转的第二页面需要注意的点</h3>
+								<p>弹性伸缩具有应突发、省成本、自动化的业务价值。平台侧将各业务零散
+								、闲置资源进行整合,形成一个大规模资源池，通过弹性调度、
+								库存管控技术在公司运营成本和业务体感中寻求较好的平衡。</p>
+								<span>微信云</span> <span>2021-</span> <span>13</span> <span>13</span> <span>2.9</span><br>
+								<span>开发</span>   <span>03-24</span> <span>24</span> <span>24</span> <span>万</span>
+							</div>
+							<div class="er">
+								<img src="img/xff.png" >
+							</div>
+							
+							<div class="san">
+								<a href="###">修改</a>
+								<a href="###">删除</a>
+							</div>
+							<div class="clear">
+								
+							</div>
+							</li>
+							
+							
+							
+							<li>
+							<div class="yi">
+								<h3>鸿蒙OS起步照官网练习创建第一页面和跳转的第二页面需要注意的点</h3>
+								<p>弹性伸缩具有应突发、省成本、自动化的业务价值。平台侧将各业务零散
+								、闲置资源进行整合,形成一个大规模资源池，通过弹性调度、
+								库存管控技术在公司运营成本和业务体感中寻求较好的平衡。</p>
+								<span>微信云</span> <span>2021-</span> <span>13</span> <span>13</span> <span>2.9</span><br>
+								<span>开发</span>   <span>03-24</span> <span>24</span> <span>24</span> <span>万</span>
+							</div>
+							<div class="er">
+								<img src="img/xff.png" >
+							</div>
+							
+							<div class="san">
+								<a href="###">修改</a>
+								<a href="###">删除</a>
+							</div>
+							<div class="clear">
+								
+							</div>
+							</li>
+							
+						</ul>
+							
+						</div>
+						
+					</div>
+
+					<div class="right">
+						<div class="shang">
+							<h3>关注了 &nbsp;&nbsp;丨&nbsp;&nbsp; 关注者<br></h3>
+							<span>22</span> <span>5034</span>
+						</div>
+						
+						<div class="xia">
+							<h3>关注列表&nbsp;&nbsp;粉丝列表</h3>
+							
+							<div class="bg1">
+								<ul>
+									<li><a href="###">dusha</a></li>
+									
+									<li><a href="###">廖显东</a></li>
+									
+									<li><a href="###">廖雪峰</a></li>
+									
+									<li><a href="###">李沐</a></li>
+									
+									
+									<li><a href="###">梅启铭</a></li>
+									
+									<li><a href="###">Linus</a></li>
+									
+									<li><a href="###">乔治布鲁斯</a></li>
+								
+								</ul>
+							</div>
+							
+							
+						</div>
+						
+						
+					</div>
+
+					<div class="clear">
+
+					</div>
+
+
+
+				</div>
+
+			</div>
+
+
+		</div>
+
+
+	</body>
+
+</template>
+<style>
+/*通用类*/
+* {
+	margin: 0;
+	padding: 0;
+}
+
+body {
+	min-width: 1400px;
+	margin: 0 auto;
+	font-size: 14px;
+	background-color: #F5E9D9;
+	text-align: justify;
+	color: #000;
+}
+
+h1,
+h2,
+h3 {
+	font-weight: 500;
+}
+
+img {
+	border: none;
+}
+
+a {
+	cursor: pointer;
+	color: #000;
+	text-decoration: none;
+	outline: none;
+}
+
+a:hover {
+	color: #f60;
+}
+
+ul {
+	list-style-type: none;
+}
+
+div.clear {
+	font: 0px Arial;
+	line-height: 0;
+	height: 0;
+	overflow: hidden;
+	clear: both;
+}
+
+
+
+/*公用CSS 开始*/
+.main {
+	width: 1400px;
+	margin: 0 auto;
+}
+
+.head {
+	width: 100%;
+	background-color: #EFEFEF;
+}
+
+.head1 {
+	padding: 20px 0;
+}
+
+.head1 .logo {
+	width: 10%;
+	float: left;
+}
+
+.head1 .geren {
+	width: 18%;
+	float: left;
+}
+
+.head1 .geren ul li {
+	width: 49%;
+	float: left;
+}
+
+.head1 .geren ul li a {
+	background-color: #FFFFFF;
+	height: 50px;
+	display: block;
+	text-align: center;
+	line-height: 50px;
+	border: 1px solid #515151;
+}
+
+.head1 .geren ul li .a1 {
+	background: url(../images/fz.png)no-repeat left;
+	background-color: #FFFFFF;
+	background-size: 18px;
+	background-position-x: 5px;
+}
+
+.head1 .geren ul li .a2 {
+	background: url(../images/grzx.png)no-repeat left;
+	background-color: #FFFFFF;
+	background-size: 18px;
+	background-position-x: 5px;
+}
+
+.head1 .geren ul li a:hover {
+	background-color: #E7F8FF;
+}
+
+.head1 .input {
+	width: 25%;
+	float: left;
+	margin: 0 20px;
+	margin-top: 10px;
+}
+
+.head1 .input .b1 {
+	width: 310px;
+	float: left;
+	height: 30px;
+}
+
+.head1 .input .b2 {
+	width: 20px;
+	float: right;
+}
+
+.head1 .san {
+	width: 10%;
+	float: left;
+	margin: 0 30px;
+	margin-top: 10px;
+}
+
+.head1 .san a {
+	display: block;
+	background-color: #B63927;
+	color: #FFFFFF;
+	text-align: center;
+	height: 30px;
+	line-height: 30px;
+	text-align: center;
+}
+
+.head1 .si {
+	width: 10%;
+	float: left;
+	text-align: center;
+}
+
+.head1 .si img {
+	width: 60px;
+	border-radius: 100%;
+}
+
+
+.head1 .wu {
+	width: 10%;
+	float: left;
+	margin-top: 10px;
+}
+
+.head1 .wu a {
+	display: block;
+	background-color: #4E93DE;
+	color: #000;
+	text-align: center;
+	height: 40px;
+	line-height: 40px;
+	text-align: center;
+	border-radius: 15px;
+}
+
+
+.centen {
+	background-color: #F5E9D9;
+	margin-top: 50px;
+}
+
+.centen1 .left {
+	width: 20%;
+	float: left;
+}
+
+.centen1 .left .aw1 {
+	text-align: center;
+	margin: 0 auto;
+	margin-bottom: 40px;
+}
+
+.centen1 .left span {
+	width: 200px;
+	height: 60px;
+	line-height: 60px;
+	color: #A0404E;
+	border: 1px solid #A0404E;
+	display: block;
+	text-align: center;
+	background-color: #FFFFFF;
+	border-radius: 15px;
+}
+
+.centen1 .left p {
+	background-color: #FFFFFF;
+	padding: 20px;
+	box-sizing: border-box;
+	font-size: 17px;
+}
+
+.centen1 .zhong {
+	width: 50%;
+	float: left;
+	margin: 0 40px;
+}
+
+.centen1 .zhong .shang {
+	background-color: #FFFFFF;
+	height: 300px;
+}
+
+.centen1 .zhong .shang .zuo {
+	width: 20%;
+	float: left;
+	text-align: center;
+}
+
+.centen1 .zhong .shang .zuo img {
+	margin-top: 80px;
+	width: 100px;
+}
+
+.centen1 .zhong .shang .zhong {
+	width: 50%;
+	float: left;
+	margin: 0 10px;
+}
+
+.centen1 .zhong .shang .zhong h3 {
+	margin-top: 50px;
+}
+
+.centen1 .zhong .shang .zhong img {
+	display: block;
+	
+	margin: 5px 0;
+}
+
+.centen1 .zhong .shang .zhong span {
+	display: inline-block;
+	width: 86px;
+	height: 20px;
+	border: 1px solid #ccc;
+	margin: 5px 0;
+	color: #4E93DE;
+	font-size: 18px;
+	text-align: center;
+	font-weight: bold;
+}
+
+.centen1 .zhong .shang .zhong p {
+	border: 1px solid #515151;
+	padding: 30px;
+	margin-top: 20px;
+}
+
+.centen1 .zhong .shang .you {
+	width: 20%;
+	float: left;
+	margin-left: 20px;
+	margin-top: 190px;
+}
+
+.centen1 .zhong .shang .you a {
+	display: inline-block;
+	width: 100px;
+	height: 50px;
+	line-height: 50px;
+	border: 1px solid #ABCCDD;
+	color: #ABCCDD;
+	text-align: center;
+	font-size: 20px;
+	font-weight: bold;
+}
+
+
+.centen1 .zhong .xia{background-color: #FFFFFF;margin-top: 30px;padding-bottom: 100px;}
+.centen1 .zhong .xia ul li{padding: 15px;box-sizing: border-box;}
+.centen1 .zhong .xia ul li .yi{width: 50%;float: left;}
+.centen1 .zhong .xia ul li .yi h3{font-size: 14px;font-weight: bold;}
+.centen1 .zhong .xia ul li .yi p{font-size: 12px;color: #515151;margin: 10px 0;}
+.centen1 .zhong .xia ul li .yi span{display: inline-block;width: 45px;}
+
+.centen1 .zhong .xia ul li .er{width: 35%;float: left;margin: 0 15px;}
+.centen1 .zhong .xia ul li .er img{width:100%;}
+.centen1 .zhong .xia ul li .san{width: 8%;float: left;}
+.centen1 .zhong .xia ul li .san a{display: block;margin-top: 20px;}
+
+.centen1 .right{width: 20%;float: right;}
+
+.centen1 .right .shang{background-color: #FFFFFF;padding: 20px 0;}
+.centen1 .right .shang h3{text-align: center;font-size: 22px;}
+.centen1 .right .shang  span{font-size: 22px;width: 48%;text-align: center;display: inline-block;}
+
+.centen1 .right .xia{text-align: center;font-size: 22px;margin: 30px 0;}
+
+.centen1 .right .xia .bg1{background-color: #FFFFFF;padding: 15px;margin-top: 20px;height: 800px;}
+.centen1 .right .xia .bg1 ul li{width: 100%;margin-top: 15px;}
+.centen1 .right .xia .bg1 ul li a{display: block;background: url(../images/grzx.png)no-repeat left;
+padding-left: 20px;
+background-size: 19px;
+text-align: left;padding-left: 30px;}	
+</style>
